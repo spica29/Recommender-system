18e2ba0f036928b303190422ffcee3cfab81f362
Amela Špica <amela.spica@gmail.com>
2018-05-31 00:09:31 +0200

    added more challenge sets

--- challenge.py
+++ challenge.py
@@ -2,29 +2,28 @@ import json
 from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
-with open('challenge.pt1.json') as f:
+with open('challenge.pt6.json') as f:
     challenge = json.load(f)
-es.indices.delete(index='challenge')
-es.indices.create(index='challenge', ignore=400)
+#es.indices.delete(index='challenge')
+#es.indices.create(index='challenge', ignore=400)
 
 more_like_string = ""
 
 for track in challenge["tracks"]:
-    # index name is playlists
-    #es.index(index='playlists', doc_type="playlist", body=playlist)
-
     more_like_string += track["artist_name"] + " " + track["track_name"] + " "
 
 print "More like " + more_like_string
 query = "{ \"query\" :{\"more_like_this\" : {\"fields\" : [\"tracks.artist_name\", \"tracks.track_name\"], \"like\":\"" + more_like_string + "\",\"min_term_freq\" : 1,\"max_query_terms\" : 100}}}"
 print "query: " + query
-res = es.search(index="playlists", body=query, size=300)
+res = es.search(index="playlists", body=query, size=100)
 print "Result: "
 score_results = ""
 for hit in res['hits']['hits']:
     playlist_name = hit['_source'].get('name')
     playlist_score = hit['_score']
-    score_results += "playlist name: " + str(playlist_name) + ", playlist score " + str(playlist_score) + "\n"
+    #print playlist_name
+    #print str(playlist_score)
+    score_results += "playlist name: " + playlist_name.encode('utf-8').strip() + ", playlist score " + str(round(playlist_score, 5)) + "\n"
 
 with open("results.txt", "w") as text_file:
     text_file.write(score_results )

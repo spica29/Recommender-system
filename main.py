d3f4817ae0029458bac082d2dce07435752f152e
Amela Špica <amela.spica@gmail.com>
2018-06-03 17:37:45 +0200

    presentation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import challenge
 
-challenge_file = 'challenge/challenge.pt20.json'
-results_file = "results/results20.txt"
+challenge_file = 'challenge/challenge.pt8.json'
+results_file = "results/results8.txt"
 
 challenge.index_songs_from_similar_playlist(challenge_file, results_file)
 challenge.songs_popularity_file(results_file)

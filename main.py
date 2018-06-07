f644853b04f897b5fae3f93cbd018d43a9748440
Amela Špica <amela.spica@gmail.com>
2018-06-07 20:18:29 +0200

    more results

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import challenge
 
-challenge_file = 'challenge/challenge.pt8.json'
-results_file = "results/results8.txt"
+challenge_file = 'challenge/challenge.pt25.json'
+results_file = "results/result25.txt"
 
 challenge.index_songs_from_similar_playlist(challenge_file, results_file)
 challenge.songs_popularity_file(results_file)
